7e4a65095edbabd8026f9198551bc58785519b2e
oushesh <oushesh@gmail.com>
2021-05-14 22:09:40 +0200

    updates to python 3

--- data/get_inria3D.py
+++ data/get_inria3D.py
@@ -1,4 +1,4 @@
-# Script to download, untar and extract the inria dataset 
+# Script to download, untar and extract the inria dataset
 
 import sys
 import shutil
@@ -42,7 +42,7 @@ def download_inria():
 		f = open(file_name, 'wb')
 		meta = u.info()
 		file_size = int(meta.getheaders("Content-Length")[0])
-		print "Downloading: %s Bytes: %s" % (file_name, file_size)
+		print ("Downloading: %s Bytes: %s" % (file_name, file_size))
 
 		file_size_dl = 0
 		block_sz = 8192
@@ -55,7 +55,7 @@ def download_inria():
 		    f.write(buffer)
 		    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
 		    status = status + chr(8)*(len(status)+1)
-		    print status,
+		    print (status)
 
 		f.close()
 
@@ -64,19 +64,19 @@ def extract_tar():
 	dir_name = os.getcwd()
 	extension = ".tar"
 
-	print "Extracting all .tar directories ..."
+	print ("Extracting all .tar directories ...")
 
-	for item in os.listdir(dir_name): 
+	for item in os.listdir(dir_name):
 		if item.endswith(extension):
-			file_name = os.path.abspath(item) 
+			file_name = os.path.abspath(item)
 			tar_ref = tarfile.open(file_name)
-			tar_ref.extractall() 
-			tar_ref.close() 
-			os.remove(file_name) 
-			print "Untarred: " + item
+			tar_ref.extractall()
+			tar_ref.close()
+			os.remove(file_name)
+			print ("Untarred: " + item)
 
 	#Remove Viz Folders with only generic jpg frames
-	print "Removing Viz Folders"
+	print ("Removing Viz Folders")
 	dir_name = os.getcwd()
 	try:
 		viz1_path = dir_name+"/inria_stereo_dataset/poseestimation_test/visualization"
@@ -88,7 +88,7 @@ def extract_tar():
 
 
 def load_inria_frame(dims = (180,320), terminate = None, prob = 0.2):
-	print "Collecting Inria Dataset Frames"
+	print ("Collecting Inria Dataset Frames")
 
 	# Recursively walk down directorie and collect Frames and concat to X and Y
 	cur_dir = os.getcwd()
@@ -105,26 +105,26 @@ def load_inria_frame(dims = (180,320), terminate = None, prob = 0.2):
 		os.makedirs(os.path.join(cur_dir, "frames", "test", "right"))
 
 	# Calculate Total Frames
-	count = 0 
-	tot_count = 0 
+	count = 0
+	tot_count = 0
 
 	for root, dirs, files in os.walk(cur_dir, topdown=False):
 		for file in files:
-			if file.endswith(l_ext): 
+			if file.endswith(l_ext):
 				tot_count = tot_count + 2
 
 
 	terminate_count = tot_count
 	if terminate is not None:
 		if terminate * 2 > tot_count:
-			print "Error: Requested number of pairs larger than total number of pairs avail"
+			print ("Error: Requested number of pairs larger than total number of pairs avail")
 			return
 		terminate_count = terminate * 2
 
 
 
-	print "Collecting " + str(terminate_count) +  " frames and downsampling them."
-	print "This will take awhile, be patient =D."
+	print ("Collecting " + str(terminate_count) +  " frames and downsampling them.")
+	print ("This will take awhile, be patient =D.")
 
 
 	# Create seeded mask for determining Test/Train Split
@@ -135,34 +135,34 @@ def load_inria_frame(dims = (180,320), terminate = None, prob = 0.2):
 	# The actual walk
 	for root, dirs, files in os.walk(cur_dir, topdown = True):
 		for file in files:
-			if file.endswith(l_ext): 
+			if file.endswith(l_ext):
 				path = os.path.join(root, file)
 
 				try:
 					l_frame = resize_scale_img(path, dims)
 					r_frame = resize_scale_img(path+r_ext, dims)
-					
-					# Write back to disk in designated Folder 
+
+					# Write back to disk in designated Folder
 					if test_mask[count/2] == 0:
 						folder = "train"
 					else:
 						folder = "test"
-						
+
 					l_path = os.path.join(cur_dir, "frames", folder, "left", str(count/2)+out_ext)
 					r_path = os.path.join(cur_dir, "frames", folder, "right", str(count/2)+out_ext)
 					skimage.io.imsave(l_path, l_frame)
 					skimage.io.imsave(r_path, r_frame)
 					count = count + 2
-				except IOError:	
-					print "IO Error (Missing Right Frame/Mem Limit/etc) , Ignoring Entry"
+				except IOError:
+					print ("IO Error (Missing Right Frame/Mem Limit/etc) , Ignoring Entry")
 
 				if count % 250 == 0 and count != 0:
-					print  "(" + str(count) + "/" + str(terminate_count) + ")"
+					print  ("(" + str(count) + "/" + str(terminate_count) + ")")
 
 				if count == terminate_count:
-					break 
+					break
 		if count == terminate_count:
-			break 
+			break
 
 	return
 
@@ -174,7 +174,7 @@ def write_to_disk(X, Y, f_name, ds_ext):
 	h5f.create_dataset('X_'+ds_ext, data=X)
 	h5f.create_dataset('Y_'+ds_ext, data=Y)
 	h5f.close()
-	print "Write Complete"
+	print ("Write Complete")
 
 # -------------------------------------------------------------- #
 
@@ -183,15 +183,15 @@ def main():
 	start = time.time()
 
 	if os.path.exists('inria_stereo_dataset/'):
-		print "Inria data already downloaded. Using cached files"
-	else: 
+		print ("Inria data already downloaded. Using cached files")
+	else:
 		download_inria()
 		extract_tar()
 
 	load_inria_frame()
 
 	end = time.time()
-	print "Extraction Completed... Took " + str((end - start)/60) + " minutes"
+	print ("Extraction Completed... Took " + str((end - start)/60) + " minutes")
 
 
 
