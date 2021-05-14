83b1d419ae44239280a4b75fe0f8533d866281ab
oushesh <oushesh@gmail.com>
2021-05-14 22:24:43 +0200

    updates to python 3

--- data/get_inria3D.py
+++ data/get_inria3D.py
@@ -40,7 +40,7 @@ def download_inria():
 		u = urllib.request.urlopen(url)
 		f = open(file_name, 'wb')
 		meta = u.info()
-		print ("meta",meta)
+		print ("meta",meta.getheaders)
 		file_size = int(meta.getheaders("Content-Length")[0])
 		#file_size =
 		print ("Downloading: %s Bytes: %s" % (file_name, file_size))
